4d599f06795fd4952acce2e1bafbf27432587d9b
Drew Chase <dcmanproductions@gmail.com>
2023-11-22 19:30:53 -0500

    added http request options struct

---
+++ canl/includes/HttpRequestOptions.h
@@ -0,0 +1,18 @@
+// LFInteractive LLC. (c) 2020-2024 All Rights Reserved
+#pragma once
+#include "HttpMethod.h"
+#include <map>
+#include <string>
+using std::string;
+using std::map;
+
+namespace canl
+{
+	struct HttpRequestOptions
+	{
+		string url;
+		HttpMethod method = GET;
+		string body;
+		map<string, string> headers;
+	};
+}
