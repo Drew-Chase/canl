760e3766bd1764b47171f1f498601293d456a3d9
Drew Chase <dcmanproductions@gmail.com>
2023-11-22 19:30:43 -0500

    added http methods enumeration

---
+++ canl/includes/HttpMethod.h
@@ -0,0 +1,17 @@
+// LFInteractive LLC. (c) 2020-2024 All Rights Reserved
+#pragma once
+namespace canl
+{
+	enum HttpMethod
+	{
+		GET,
+		POST,
+		PUT,
+		DELETE,
+		HEAD,
+		CONNECT,
+		OPTIONS,
+		TRACE,
+		PATCH
+	};
+}
