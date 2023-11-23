0b8b9b6fa395970eedfecf402504f63928c68c4b
Drew Chase <dcmanproductions@gmail.com>
2023-11-22 20:59:17 -0500

    added test code

--- example/main.cpp
+++ example/main.cpp
@@ -1,7 +1,26 @@
+// LFInteractive LLC. (c) 2020-2024 All Rights Reserved
+#include "NetworkClient.h"
 #include <stdio.h>
-
+using namespace canl;
 int main()
 {
-    printf("Hello World!\n");
-    return 0;
+	auto client = new NetworkClient();
+	HttpResponseMessage response = client->get("https://www.google.com", [](NetworkUpdateMessage message)
+	{
+		printf("Bytes received: %d\n", message.bytesReceived);
+		printf("Bytes total: %d\n", message.bytesTotal);
+		printf("Bytes remaining: %d\n", message.bytesRemaining);
+		printf("Bytes per second: %d\n", message.bytesPerSecond);
+		printf("Last callback time: %d\n", message.lastCallbackTime);
+		printf("Path: %s\n", message.path.c_str());
+		printf("File name: %s\n", message.fileName.c_str());
+		printf("Percentage: %d\n", message.percentage);
+	});
+
+	if (response.isSuccessStatusCode)
+	{
+		printf("Content: %s", response.body);
+	}
+
+	return 0;
 }
