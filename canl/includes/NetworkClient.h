10c4d602263fc5afc81fcb00a14163db8b9d2964
Drew Chase <dcmanproductions@gmail.com>
2023-11-22 19:31:16 -0500

    added network client class

---
+++ canl/includes/NetworkClient.h
@@ -0,0 +1,12 @@
+// LFInteractive LLC. (c) 2020-2024 All Rights Reserved
+#pragma once
+namespace canl
+{
+	class NetworkClient
+	{
+	public:
+		NetworkClient();
+		~NetworkClient();
+		void close();
+	};
+}
