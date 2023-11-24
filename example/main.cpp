e07b0027194e87fe7ee7ad4f5b6e8fcd2553a3fa
Drew Chase <dcmanproductions@gmail.com>
2023-11-23 19:55:15 -0500

    updated test

--- example/main.cpp
+++ example/main.cpp
@@ -5,17 +5,7 @@ using namespace canl;
 int main()
 {
 	auto client = new NetworkClient();
-	HttpResponseMessage response = client->get("https://www.google.com", [](NetworkUpdateMessage message)
-	{
-		printf("Bytes received: %d\n", message.bytesReceived);
-		printf("Bytes total: %d\n", message.bytesTotal);
-		printf("Bytes remaining: %d\n", message.bytesRemaining);
-		printf("Bytes per second: %d\n", message.bytesPerSecond);
-		printf("Last callback time: %d\n", message.lastCallbackTime);
-		printf("Path: %s\n", message.path.c_str());
-		printf("File name: %s\n", message.fileName.c_str());
-		printf("Percentage: %d\n", message.percentage);
-	});
+	HttpResponseMessage response = client->get("https://www.google.com");
 
 	if (response.isSuccessStatusCode)
 	{
